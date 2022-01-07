7225a2a2376ac1f563957b691724f22c76ae9154
Mahdi Varposhti <varp2010m@gmail.com>
2022-01-07 13:36:11 -0700

    Merge frontend & backend

---
+++ backend/server.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, flash, request, redirect
+from werkzeug.utils import secure_filename
+from summarizer import summarize
+from cloudVision import detect_text
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def home():
+    return "<p>Hello, World!</p>"
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        text = summarize(detect_text(filename))
+        os.remove(filename)
+        return text
