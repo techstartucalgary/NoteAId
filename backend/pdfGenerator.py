45a65ee32cc5e5320aa9ff99c426107ac7982b78
Mahdi Varposhti <varp2010m@gmail.com>
2022-04-13 23:10:54 -0600

    Add stopwords nltk package to Dockerfile

--- backend/pdfGenerator.py
+++ backend/pdfGenerator.py
@@ -1,10 +1,10 @@
 from fpdf import FPDF
 
-def pdfGenerator(text: str, fileName: str = "PDFOutput") -> None:
+def generate_pdf(text: str) -> None:
     txt = text.encode('latin-1', 'replace').decode('latin-1')
     pdf = FPDF()
+    pdf.set_margins(20, 15, 20)
     pdf.add_page()
     pdf.set_font('Arial', size=12)
-    pdf.multi_cell(0, 10, txt=txt, border=0, align='L')
-    pdf.output(f'{fileName}.pdf')
-    return None
+    pdf.multi_cell(0, 10, txt=txt, border=0, align='J')
+    return pdf.output(dest='S').encode('latin-1')
